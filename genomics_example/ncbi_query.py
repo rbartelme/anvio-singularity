e1d5b4dd817daf8bf9292a1772ce793ce0a56f53
rbartelme <r.p.bartelme@gmail.com>
2021-03-12 11:58:17 -0700

    pseudo coded biopython query to make genomic analyses more reproducible

---
+++ genomics_example/ncbi_query.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# import packages
+
+# organism id as input
+
+# define function to get fasta & metadata for all that match the ncbi organism id
+
+    # get human readable names for each result
+    
+    # sub `_` for whitespace in organism names
+
+    # pull associated fasta and metadata
+
+    # write out anvi'o snakemake tsv input format
+
+    # write out ncbi metadata for downstream analyses
