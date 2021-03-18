0092dd6c28e620dccc039a90433b1046d8c8453b
rbartelme <r.p.bartelme@gmail.com>
2021-03-18 13:40:20 -0700

    updated script pseudocode

--- pangenomics_example/ncbi_query.py
+++ pangenomics_example/ncbi_query.py
@@ -2,16 +2,20 @@
 
 # import packages
 from Bio import Entrez
-# organism id as input
+# input organism id from cli flag as an array
+
+    #define test for array input
+    #function to query ncbi by tax id
 
 # define function to get fasta & metadata for all that match the ncbi organism id
 
-    # get human readable names for each result
+    # get human readable spcies names for each result
     
-    # sub `_` for whitespace in organism names
+        # sub `_` for whitespace in organism names
+        # write out ncbi metadata for downstream analyses
 
     # pull associated fasta and metadata
 
-    # write out anvi'o snakemake tsv input format
+    # write out anvi'o snakemake tsv input format i.e. organism_name_here`\t`fasta_file_here.fa
 
-    # write out ncbi metadata for downstream analyses
+    
